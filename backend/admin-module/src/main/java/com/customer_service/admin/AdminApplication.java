db1f54570238751c9f5baad7cce42a8cb94fc060
wincLei <923890991@qq.com>
2026-01-21 15:21:33 +0800

    修复后端的配置

--- backend/admin-module/src/main/java/com/customer_service/admin/AdminApplication.java
+++ backend/admin-module/src/main/java/com/customer_service/admin/AdminApplication.java
@@ -2,11 +2,15 @@ package com.customer_service.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.customer_service" })
+@EntityScan(basePackages = { "com.customer_service.shared.entity" })
+@EnableJpaRepositories(basePackages = { "com.customer_service" })
 @EnableScheduling
 public class AdminApplication {
     public static void main(String[] args) {
--- backend/portal-module/src/main/java/com/customer_service/portal/PortalApplication.java
+++ backend/portal-module/src/main/java/com/customer_service/portal/PortalApplication.java
@@ -2,10 +2,14 @@ package com.customer_service.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.customer_service" })
+@EntityScan(basePackages = { "com.customer_service.shared.entity" })
+@EnableJpaRepositories(basePackages = { "com.customer_service" })
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
